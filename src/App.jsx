cb6a514bdb88933a6e6ce9cfb69254b8fa6c3350
kishore-kumar-ss <158063464+kishore-kumar-ss@users.noreply.github.com>
2024-04-20 00:12:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import AboutUs from './AboutUs';
 import './App.css';
 
 import cycleImage from '../images/cycle.jpg';
+import kettleImage from '../images/kettle.jpg';
+import umbrellaImage from '../images/umbrella.jpg';
+import mattressImage from '../images/mattress.jpg';
+import mousepadImage from '../images/mousePad.jpg';
+import extenderImage from '../images/extender.jpg';
+import ironImage from '../images/iron.jpg';
+import calculatorImage from '../images/calculator.jpg';
+import chargerImage from '../images/charger.jpg';
+import shoesImage from '../images/shoes.jpg';
+import bagImage from '../images/bag.jpg';
+import bottleImage from '../images/bottle.jpg';
 
 function App() {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
@@ -15,18 +26,18 @@ function App() {
   const [isaboutopen, setabout] = useState(false);
 
   const items = [
-    { name: 'Kettle', price: 800, imageURL: '../images/kettle.jpg', starRating: 4.3, description: 'safe to use. please keep children away.' },
-    { name: 'Umbrella', price: 600, imageURL: '../images/umbrella.jpg', starRating: 4.6, description: 'good quality!👌' },
+    { name: 'Kettle', price: 800, imageURL: kettleImage, starRating: 4.3, description: 'safe to use. please keep children away.' },
+    { name: 'Umbrella', price: 600, imageURL: umbrellaImage, starRating: 4.6, description: 'good quality!👌' },
     { name: 'Cycle', price: 7000, imageURL: cycleImage, starRating: 4, description: 'Good quality with 10% off' },
-    { name: 'Mattress', price: 4500, imageURL: '../images/mattress.jpg', starRating: 3, description: 'Sleepwell mattress, for good dreams 😴' },
-    { name: 'Mouse pad', price: 200, imageURL: '../images/mousePad.jpg', starRating: 4.3, description: 'Redgear pad used for 2 years.' },
-    { name: 'Extender', price: 350, imageURL: '../images/extender.jpg', starRating: 3.6, description: '1 year warrenty' },
-    { name: 'Iron', price: 1200, imageURL: '../images/iron.jpg', starRating: 2.4, description: 'Works great with 50% off' },
-    { name: 'Calculator', price: 750, imageURL: '../images/calculator.jpg', starRating: 4.9, description: 'Casio scientific calculator' },
-    { name: 'Charger', price: 900, imageURL: '../images/charger.jpg', starRating: 5, description: 'USB-c type 65 watt charger samsung' },
-    { name: 'Shoes', price: 80000, imageURL: '../images/shoes.jpg', starRating: 5, description: 'Adidas air size 11 ' },
-    { name: 'Bag', price: 2300, imageURL: '../images/bag.jpg', starRating: 4.6, description: 'Wildcraft bag comes with raincover and charging ports' },
-    { name: 'Bottle', price: 650, imageURL: '../images/bottle.jpg', starRating: 3.9, description: 'Stainless steel 1L water bottle' }
+    { name: 'Mattress', price: 4500, imageURL: mattressImage, starRating: 3, description: 'Sleepwell mattress, for good dreams 😴' },
+    { name: 'Mouse pad', price: 200, imageURL: mousepadImage, starRating: 4.3, description: 'Redgear pad used for 2 years.' },
+    { name: 'Extender', price: 350, imageURL: extenderImage, starRating: 3.6, description: '1 year warrenty' },
+    { name: 'Iron', price: 1200, imageURL: ironImage, starRating: 2.4, description: 'Works great with 50% off' },
+    { name: 'Calculator', price: 750, imageURL: calculatorImage, starRating: 4.9, description: 'Casio scientific calculator' },
+    { name: 'Charger', price: 900, imageURL: chargerImage, starRating: 5, description: 'USB-c type 65 watt charger samsung' },
+    { name: 'Shoes', price: 80000, imageURL: shoesImage, starRating: 5, description: 'Adidas air size 11 ' },
+    { name: 'Bag', price: 2300, imageURL: bagImage, starRating: 4.6, description: 'Wildcraft bag comes with raincover and charging ports' },
+    { name: 'Bottle', price: 650, imageURL: bottleImage, starRating: 3.9, description: 'Stainless steel 1L water bottle' }
   ];
 
   const toggleSignUp = () => {
